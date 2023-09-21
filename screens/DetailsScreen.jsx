b8c32fa3d4e1ff799b171d1599dbb5ce41762f62
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2023-09-21 10:29:06 +0200

    Update DetailsScreen.jsx

--- screens/DetailsScreen.jsx
+++ screens/DetailsScreen.jsx
@@ -3,11 +3,9 @@ import data from '../assets/data/data.json';
 import UserDetail from '../components/UserDetail';
 
 function DetailsScreen({ route, navigation }) {
-  const { itemId } = route.params;
+  const { itemId } = route.params;  
   if (itemId === undefined) navigation.navigate('HomeScreen');
-  
-  const usersData = data.users;
-  const userData = usersData.filter(item => item.id === itemId);
+  const userData = data.users.filter(item => item.id === itemId);
   return <UserDetail contact={userData[0]} />;  
 }
 
